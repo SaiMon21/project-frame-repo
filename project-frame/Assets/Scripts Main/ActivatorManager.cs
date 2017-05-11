9f686a145f3c7c5b69521ff10c04f39fed498edd
SaiMon21 <niru.teracino@gmail.com>
2017-05-12 00:20:19 +1000

    Cam + Trigger Test
    - Added: Several camera and triggers for tests.

---
+++ project-frame/Assets/Scripts Main/ActivatorManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivatorManager : MonoBehaviour {
+
+	public GameObject[] objToActivate;
+	public GameObject[] objToDeactivate;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void activateObjects(){
+		foreach (GameObject objects in objToActivate) {
+			objects.SetActive (true);
+			Debug.Log ("Objects in array are now ACTIVATED");
+		}
+	}
+
+
+	public void deactivateObjects(){
+		foreach (GameObject objects in objToDeactivate) {
+			objects.SetActive (false);
+			Debug.Log ("Objects in array are now DE-ACTIVATED");
+		}
+	}
+
+
+
+
+
+}
